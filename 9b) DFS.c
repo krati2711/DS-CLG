bd50653b1dc90096a11e65d9e7ec7bb8f5d12665
krati2711 <kratigoel.cs24@bmsce.ac.in>
2025-12-23 11:56:16 +0530

    Create 9b) DFS.c

---
+++ 9b) DFS.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+
+int n;
+int graph[20][20];
+int visited[20];
+
+void dfs(int v)
+{
+    int i;
+    visited[v] = 1;
+    for(i=0;i<n;i++)
+    {
+        if (graph[v][i] == 1 && visited[i] == 0){
+                dfs(i);
+        }
+    }
+}
+
+int main1()
+{
+    int i, j, connected = 1;
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+    printf("Enter adjacency matrix:\n");
+    for (i = 0; i < n; i++){
+            for (j = 0; j < n; j++){
+                    scanf("%d", &graph[i][j]);
+            }
+            visited[i] = 0;
+    }
+    dfs(0);
+    for (i = 0; i < n; i++)
+    {
+        if (visited[i] == 0)
+        {
+            connected = 0;
+            break;
+        }
+    }
+
+    if (connected) printf("The graph is CONNECTED\n");
+    else printf("The graph is NOT CONNECTED\n");
+    return 0;
+}
